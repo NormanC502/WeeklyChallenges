c8e15f78dee37f4b56c8a21145d809a2898bc011
Norman Calhoun <shakauluz73@yahoo.com>
2021-02-21 10:44:06 -0500

    week2 complete

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 
 namespace ChallengesWithTestsMark8
@@ -8,12 +9,16 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'); ;
+            return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            var i = vals.Length;
+                if (i % 2 == 0)
+                    return true;
+                else
+                    return false;
 
         }
 
@@ -39,32 +44,90 @@ namespace ChallengesWithTestsMark8
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
-        }
+            if (numbers == null) 
+            {
+                return 0;
+            }
+            double minimum = 0;
+            double maximum = 0;
+            double sum = 0;
+            foreach (double num in numbers) 
+            {
+                minimum = numbers.Min();
+                maximum = numbers.Max();
+                sum = minimum + maximum;
+            }
+            return sum;
+            
+        }   
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            if (str1.Length < str2.Length) 
+            {
+                return str1.Length;
+            }
+            return str2.Length;
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+            //int sum = numbers.Sum(); using Sum method instead of numbers.Length value adding with iteration throung collection
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
-        }
+            if (numbers == null)
+            {
+                return 0;
+            }
 
+            var sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {                
+                if (numbers[i] % 2 == 0)
+                {
+                     sum += numbers[i];
+                    
+                }
+                    
+            }
+            return sum;
+
+            // return numbers.Where(x => x % 2 == 0).Sum(); using Ling with Where numbers are divisible by 2 and Sum method.
+        }
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
-        }
+            if (numbers == null)
+            {
+                return false;
+            }
+            return (numbers.Sum() % 2 != 0);    // returns true statement if numbers not divisble by 2        
+        }           
+          
+        
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            if (number <= 0)
+            {
+                return 0;
+            }
+            else 
+            {
+                return number / 2;
+            }
         }
     }
 }
